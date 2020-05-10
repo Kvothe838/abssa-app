a51be80bba098ab204880b39a1fd06c487f2f5f0
Kvothe838 <facundoyuffrida@gmail.com>
2020-05-10 15:21:41 -0300

    Armado de header yeader y sidenav. Creacion de componentes para cada opción del menú.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsComponent } from './components/us/us.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	 { path: 'home', component: HomeComponent},
+	 { path: '', redirectTo: '/home', pathMatch: 'full' },
+	 { path: 'clients', component: ClientsComponent},
+	 { path: 'contact', component: ContactComponent},
+	 { path: 'us', component: UsComponent},
+	 { path: 'services', component: ServicesComponent},	 
+	 { path: 'solutions', component: SolutionsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from  '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { UsComponent } from './components/us/us.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UsComponent,
+    SolutionsComponent,
+    ServicesComponent,
+    ClientsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
